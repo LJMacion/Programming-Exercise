1677d45928b952cfdd22b021aca2883ee1bd1041
LJMacion <159424278+LJMacion@users.noreply.github.com>
2024-04-11 22:20:03 +0800

    Add files via upload

---
+++ Vehicle.java
@@ -0,0 +1,46 @@
+
+class Vehicle {
+    private String regNo;
+    private String make;
+    private int yearOfManufacture;
+    private double value;
+
+    public Vehicle(String regNo, String make, int yearOfManufacture, double value) {
+        this.regNo = regNo;
+        // complete this assignment statements here
+        this.make = make;
+        this.yearOfManufacture = yearOfManufacture;
+        this.value = value;
+
+    }
+    
+    // write getter and setter here
+    public String getRegNo() {
+         return this.regNo;
+    }
+    
+    public String getMake() {
+         return this.make;
+    }
+    
+    public int getYearOfManufacture () {
+         return this.yearOfManufacture;
+    }
+    
+    public double getValue () {
+         return this.value;
+    }
+    
+    public void setValue (double value) {
+         this.value = value;
+    }
+
+    public int calculateAge(int currentYear) {
+         return currentYear - yearOfManufacture;
+    }
+
+    @Override
+    public String toString() {
+        return "Reg No: " + regNo + ", Make: " + make + ", Year of Manufacture: " + yearOfManufacture + ", Value: Php" + value;
+    }
+}
